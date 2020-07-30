b3b83b15a8f1f9e5ecb5cf7abc3463e4d5087886
Ash Hipgrave <AshHipgrave@users.noreply.github.com>
2020-07-30 16:31:45 +0100

    Simplified text creation in the event you're setting text later / Ensured the font file is closed once it's no longer needed.

--- RogueClient/Source/RenderText.cpp
+++ RogueClient/Source/RenderText.cpp
@@ -1,35 +1,26 @@
 #include "RenderText.h"
 
-RenderText::RenderText(SDL_Renderer* renderer, const char* fontName, const int fontSize, SDL_Color textColour)
+RenderText::RenderText(SDL_Renderer* renderer)
 {
 	m_Renderer = renderer;
-
-	m_TextFont = TTF_OpenFont(fontName, fontSize);
-
-	if (!m_TextFont)
-	{
-		std::cout << "Failed to load required font! " << TTF_GetError() << std::endl;
-		return;
-	}
 }
 
 RenderText::RenderText(const char* text, int xPos, int yPos, SDL_Renderer* renderer, const char* fontName, const int fontSize, SDL_Color textColour)
 {
 	m_Renderer = renderer;
 
-	m_TextFont = TTF_OpenFont(fontName, fontSize);
+	TTF_Font* textFont = TTF_OpenFont(fontName, fontSize);
 
-	if (!m_TextFont)
-	{
-		std::cout << "Failed to load required font! " << TTF_GetError() << std::endl;
-		return;
-	}
-
-	m_TextSurface = TTF_RenderText_Blended(m_TextFont, text, textColour);
-
+	m_TextSurface = TTF_RenderText_Blended(textFont, text, textColour);
 	m_TextTexture = SDL_CreateTextureFromSurface(renderer, m_TextSurface);
 
-	m_DestRect = { xPos, yPos, m_TextSurface->w, m_TextSurface->h };
+	m_DestRect.x = xPos;
+	m_DestRect.y = yPos;
+
+	m_DestRect.w = m_TextSurface->w;
+	m_DestRect.h = m_TextSurface->h;
+
+	TTF_CloseFont(textFont);
 }
 
 RenderText::~RenderText()
@@ -43,8 +34,6 @@ RenderText::~RenderText()
 	{
 		SDL_FreeSurface(m_TextSurface);
 	}
-
-	TTF_CloseFont(m_TextFont);
 }
 
 void RenderText::Draw()
@@ -64,12 +53,15 @@ void RenderText::SetText(const char* text, const char* fontName, const int fontS
 		SDL_FreeSurface(m_TextSurface);
 	}
 
-	m_TextSurface = TTF_RenderText_Blended(m_TextFont, text, textColour);
+	TTF_Font* textFont = TTF_OpenFont(fontName, fontSize);
 
+	m_TextSurface = TTF_RenderText_Blended(textFont, text, textColour);
 	m_TextTexture = SDL_CreateTextureFromSurface(m_Renderer, m_TextSurface);
 
 	m_DestRect.w = m_TextSurface->w; 
 	m_DestRect.h = m_TextSurface->h;
+
+	TTF_CloseFont(textFont);
 }
 
 void RenderText::SetPosition(int xPos, int yPos)
--- RogueClient/Source/RenderText.h
+++ RogueClient/Source/RenderText.h
@@ -8,28 +8,23 @@
 class RenderText
 {
 public:
-	RenderText(SDL_Renderer* renderer, const char* fontName = "C:\\Windows\\Fonts\\arial.ttf", const int fontSize = 18, SDL_Color textColour = { 0, 0, 0 });
-	
-	RenderText(const char* text, int xPos, int yPos, SDL_Renderer* renderer, const char* fontName = "C:\\Windows\\Fonts\\arial.ttf", const int fontSize = 18, SDL_Color textColour = { 0, 0, 0 });
+	RenderText(SDL_Renderer* renderer);
+	RenderText(const char* text, int xPos, int yPos, SDL_Renderer* renderer, const char* fontName = "C:\\Windows\\Fonts\\arial.ttf", const int fontSize = 18, SDL_Color textColour = { 0, 0, 0, 255 });
 	
 	~RenderText();
 
-	//void Draw(const char* text, int xPos, int yPos, SDL_Renderer* renderer, SDL_Color textColour = { 0, 0, 0 });
-
 	void Draw();
 
-	void SetText(const char* text, const char* fontName = "C:\\Windows\\Fonts\\arial.ttf", const int fontSize = 18, SDL_Color textColour = { 0, 0, 0 });
+	void SetText(const char* text, const char* fontName = "C:\\Windows\\Fonts\\arial.ttf", const int fontSize = 18, SDL_Color textColour = { 0, 0, 0, 255 });
 	void SetPosition(int xPos, int yPos);
 
 private:
-	TTF_Font* m_TextFont = nullptr;
-
 	SDL_Renderer* m_Renderer = nullptr;
 
 	SDL_Surface* m_TextSurface = nullptr;
 	SDL_Texture* m_TextTexture = nullptr;
 
 private:
-	SDL_Rect m_DestRect;
+	SDL_Rect m_DestRect = { 0, 0, 0, 0 };
 };
 
