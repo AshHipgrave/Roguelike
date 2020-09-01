4994c04408e129dc8bc7b7b985b605d13b61f69d
Ash Hipgrave <AshHipgrave@users.noreply.github.com>
2020-09-01 22:55:13 +0100

    Added console for debug output display

--- RogueClient/Source/Main.cpp
+++ RogueClient/Source/Main.cpp
@@ -1,7 +1,12 @@
 #include "RogueClient.h"
 
+#include <Windows.h>
+
 int main(int argc, char* args[])
 {
+	AllocConsole();
+	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
+
 	RogueClient client;
 
 	if (!client.Init("Game Window", 1920, 1080))
